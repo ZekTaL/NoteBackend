3aac745deee2671f79a504249e2d2f7c06a4c598
ZekTaL <francykiller@hotmail.it>
2020-09-11 16:24:12 +1000

    update clean

--- index.js
+++ index.js
@@ -140,6 +140,6 @@ app.use(errorHandler)
 // PORT LISTENING
 const PORT = process.env.PORT
 app.listen(PORT, () => {
-    console.log(`Server running on port ${PORT}`)
+  console.log(`Server running on port ${PORT}`)
 })
 
